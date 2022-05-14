7315c93075ce5d190fb48be9a0ea0edcefbf2ecb
rkwadriga <dmitry.kushneriov@obreey-products.com>
2022-05-14 13:37:55 +0300

    Create store for tasks

---
+++ src/data/stores/useToDoStore.ts
@@ -0,0 +1,45 @@
+import create from "zustand";
+import { generateId } from "../helpers";
+
+interface Task {
+    id: string;
+    title: string;
+    createdAt: number;
+}
+
+interface ToDoStore {
+    tasks: Task[];
+    createTask: (title: string) => void;
+    updateTask: (id: string, title: string) => void;
+    removeTask: (id: string) => void;
+}
+
+export const useToDoStore = create<ToDoStore>((set, get) => ({
+    tasks: [],
+    createTask: (title: string) => {
+        const { tasks } = get();
+        const newTask = {
+            id: generateId(),
+            title,
+            createdAt: (new Date()).getTime()
+        };
+        set({
+            tasks: [newTask].concat(tasks),
+        });
+    },
+    updateTask: (id: string, title: string) => {
+        const { tasks } = get();
+        set({
+            tasks: tasks.map(task => ({
+                ...task,
+                title: task.id === id ? title : task.title
+            })),
+        });
+    },
+    removeTask: (id: string) => {
+        const { tasks } = get();
+        set({
+            tasks: tasks.filter(task => task.id !== id),
+        });
+    }
+}));
--- src/views/App/index.tsx
+++ src/views/App/index.tsx
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { useEffect } from 'react';
+import { useToDoStore } from '../../data/stores/useToDoStore';
 
-export const App: React.FS = () => {
+import styles from './index.module.scss';
+
+export const App: React.FC = () => {
+    const [
+        tasks,
+        createTask,
+        updateTask,
+        removeTask
+    ] = useToDoStore(state => [
+        state.tasks,
+        state.createTask,
+        state.updateTask,
+        state.removeTask
+    ]);
+
     return (
-        <article>123345a</article>
+        <article className={styles.article}>
+            <h1 className={styles.articleTitle}>To Do App</h1>
+            <section className={styles.articleSection}></section>
+            <section className={styles.articleSection}></section>
+        </article>
     );
 };
